476329627c2b989f952a4ff6648c7ad31b80e198
Kellian <kellian.mirey.etu@univ-lille.fr>
2023-12-10 17:54:20 +0100

    Prepared base elements and resources

---
+++ src/components/TexteAccueil.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './TexteAccueil.css'
+
+export const TexteAccueil = () => {
+  return (
+    <section className='font-face-rm'>
+        <h1 className='text-darktext font-bold text-3xl lg:text-5xl'>Bonjour, Je m’appelle Kellian Mirey</h1>
+        <p className='text-darktext text-lg lg:text-2xl'>
+        Je suis actuellement étudiant en BUT Informatique 
+        dans le but de devenir développeur Fullstack. 
+        Je me suis passionné pour l’informatique depuis très jeune 
+        par les jeux vidéo et le fait de comprendre la plupart des 
+        logiciels qui m’accompagnent quotidiennement. 
+        Mon objectif en tant que développeur est de développer 
+        des logiciels efficaces et satisfaisants pour ses utilisateurs (dont moi).
+        </p>
+    </section>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightgradient1': '#B1C9EF',
+        'lightgradient2': '#5E96EE',
+        'darkgradient1': '#1C3144',
+        'darkgradient2': '#041320',
+        'lighttext': '#D9E8F7',
+        'darktext': '#081726',
+      }
+    },
   },
   plugins: [],
 }
