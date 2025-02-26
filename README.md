dff3742b88db6d7cf97969093710415ddd781113
StazzCode <StazzCode@users.noreply.github.com>
2025-02-26 19:37:00 +0000

    deploy: 61e653e86cba6d3582841396d6bad0215f4bedf3

--- README.md
+++
@@ -1,5 +0,0 @@
-# Portfolio
-
-Ceci est mon portfolio réalisé en React et TypeScript.
-
-Lien : <https://stazzcode.github.io/Portfolio>
--- src/components/iconsComponents/RightArrow.tsx
+++
@@ -1,6 +0,0 @@
-export const RightArrow = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="64" width="64" viewBox="0 0 320 512"><path opacity="1" fill="#D9E8F7" d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
-    )
-  }
-  
